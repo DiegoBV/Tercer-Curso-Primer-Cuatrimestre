a52b8d820ca111d5b33f6ce30e402b2842329352
Diego BV <diego.baratto8@gmail.com>
2018-09-28 19:52:52 +0200

    Practica 1 de física, gitignore actualizado

---
+++ Física/PracticaFisica/Practica2/Particula.cpp
@@ -0,0 +1,45 @@
+#include "Particula.h"
+#include <iostream>
+
+physx::PxShape* Particula::createShape(Shape tipo, Medidas size)
+{
+	physx::PxShape* shape_;
+
+	switch (tipo)
+	{
+	case Capsule:
+		shape_ = CreateShape(physx::PxCapsuleGeometry(size.x_, size.y_));
+		break;
+	case Sphere:
+		shape_ = CreateShape(physx::PxSphereGeometry(size.x_));
+		break;
+	case Box:
+		shape_ = CreateShape(physx::PxBoxGeometry(size.x_, size.y_, size.z_));
+		break;
+	default:
+		break;
+	}
+
+	return shape_;
+}
+
+void Particula::changeDamping(const float newDamping)
+{
+	if (newDamping >= 0 && newDamping <= 1) //comprobacion de si la nueva fuerza tiene un valor correcto
+		damping = newDamping;
+	//else ---> avisar, lanzar una excepcion...?	
+}
+
+void Particula::move_Constant_Velocity(float t)
+{
+	//e = e + v*t
+	p += v*t;
+	const physx::PxTransform* temp = transform; //???
+	transform = new physx::PxTransform(p); //???
+	delete temp; //???
+}
+
+void Particula::changeInverseMass(const float newMass)
+{
+	inverse_mass = newMass;
+}
