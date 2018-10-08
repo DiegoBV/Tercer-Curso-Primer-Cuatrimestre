edd1cf44793d00d617002bb4fd95362ac3cd56d0
Diego BV <diego.baratto8@gmail.com>
2018-10-08 16:24:38 +0200

    Ejercicio suma actualizado

--- MARP/Monticulos/Ejercicio1/Source.cpp
+++ MARP/Monticulos/Ejercicio1/Source.cpp
@@ -1,53 +1,52 @@
-#include "PriorityQueue.h"
+//DIEGO BARATTO VALDIVIA
 #include <iostream>
 #include <string>
+#include "PriorityQueue.h"
+
 using namespace std;
 
-void entrada();
-long long int resuelve(PriorityQueue<long long int>& pQueue);
+//Coste O(n), donde n es igual al numero de elementos del monticulo
+long long int resuelve(PriorityQueue<long long int>& pQueue) {
+	long long int suma_ = 0, esfuerzo_ = 0;
 
-int main() {
-	entrada();
-	return 0;
+	while (pQueue.size() != 1) {
+		long long int sumando1 = 0, sumando2 = 0;
+
+		sumando1 = pQueue.top(); //saca el primer sumando
+		pQueue.pop();
+		sumando2 = pQueue.top(); //saca el segundo sumando
+		pQueue.pop();
+
+		suma_ = sumando1 + sumando2; //los suma
+		esfuerzo_ += suma_; //calculo del esfuerzo
+
+		pQueue.push(suma_); //push de la variable suma_ en el monticulo
+	}
+
+	return esfuerzo_;
 }
 
-void entrada(){
-	int n; //numero de elementos
+void entrada() {
+	int n = 0; 
 	cin >> n;
 
-	while (n != 0)
-	{
+	while (n != 0) {
 		PriorityQueue<long long int> pQueue;
-		
-		int i = 0;
-		while (i < n) {
-			int elemento;
-			cin >> elemento;
-			pQueue.push(elemento);
-			i++;
+
+		for (int i = 0; i < n; i++) {
+			long long int elem = 0;
+			cin >> elem;
+
+			pQueue.push(elem);
 		}
 
 		cout << resuelve(pQueue) << endl;
+
 		cin >> n;
 	}
 }
 
-long long int resuelve(PriorityQueue<long long int>& pQueue){
-	long long int esfuerzo_minimo = 0;
-	
-	bool stop = false;
-
-	while (pQueue.size() != 1) {
-		long long int suma = 0;
-		suma += pQueue.top();
-		pQueue.pop();
-		suma += pQueue.top();
-		pQueue.pop();
-
-		esfuerzo_minimo += suma;
-
-		pQueue.push(suma);
-	}
-
-	return esfuerzo_minimo;
+int main() {
+	entrada();
+	return 0;
 }
