c8a410563a22cb7b450c5a366e45b25e051b9728
Diego BV <diego.baratto8@gmail.com>
2018-10-08 16:57:48 +0200

    Ejercicio UCM actualizado

--- MARP/Monticulos/Ejercicio2/Source.cpp
+++ MARP/Monticulos/Ejercicio2/Source.cpp
@@ -1,68 +1,51 @@
 //DIEGO BARATTO VALDIVIA
-#include "PriorityQueue.h"
 #include <iostream>
 #include <string>
+#include "PriorityQueue.h"
+#include <vector>
 
 using namespace std;
 
 //struct del usuario con la informacion necesaria para el problema
-struct User
-{
+struct User {
 	int id_;
-	int periodoActual_;
-	int periodoInicial_;
+	int periodo_;
+	int tInicial_;
 
-	User() : id_(0), periodoActual_(0), periodoInicial_(0) {};
-	User(int id, int periodoInicial) : id_(id), periodoActual_(periodoInicial), periodoInicial_(periodoInicial) {};
+	User(int id, int t) : id_(id), periodo_(t), tInicial_(t) {};
+	User(): id_(0), periodo_(0), tInicial_(0) {};
 };
 
 //operador comparador
-struct Comp
-{
-	bool operator()(const User& user1, const User& user2) const
-	{
-		if (user1.periodoActual_ < user2.periodoActual_) {
-			return true;
-		}
-		else if (user1.periodoActual_ == user2.periodoActual_) {
-			if (user1.id_ < user2.id_) {
-				return true;
-			}
-			else {
-				return false;
-			}
-		}
-		else {
-			return false;
-		}
-	
+struct Comparador {
+	bool operator()(const User& u1, const User& u2) const {
+		return (u1.periodo_ < u2.periodo_) || (u1.periodo_ == u2.periodo_ && u1.id_ < u2.id_);
 	}
 };
 
 //coste O(k), siendo k el numero de envios que se quieren comprobar
-void resuelve(PriorityQueue<User, Comp>& pQueue, const int& k) {
+void resuelve(PriorityQueue<User, Comparador>& pQueue, const int& k) {
 	for (int i = 0; i < k; i++) {
 		//el top se saca del monticulo (el usuario con la prioridad mas alta --> periodo mas pequeño)
-		User usuario = pQueue.top();
+		User actual_user = pQueue.top(); 
 		pQueue.pop();
 
 		//se muestra por pantalla
-		cout << usuario.id_ << endl;
+		cout << actual_user.id_ << endl;
 
 		//se introduce de nuevo en el monticulo con un nuevo periodo
-		usuario.periodoActual_ += usuario.periodoInicial_;
-		pQueue.push(usuario);
+		actual_user.periodo_ += actual_user.tInicial_;
+		pQueue.push(actual_user);
 	}
 
 	cout << "----" << endl;
 }
-
 void entrada() {
 	int n = 0;
 	cin >> n;
 
 	while (n != 0) {
-		PriorityQueue<User, Comp> pQueue;
+		PriorityQueue<User, Comparador> pQueue;
 
 		for (int i = 0; i < n; i++) {
 			int id, periodo;
@@ -71,13 +54,14 @@ void entrada() {
 			pQueue.push(User(id, periodo));
 		}
 
-		int k = 0;
+		int k;
 		cin >> k;
 
 		resuelve(pQueue, k);
 
 		cin >> n;
 	}
+
 }
 
 int main() {
