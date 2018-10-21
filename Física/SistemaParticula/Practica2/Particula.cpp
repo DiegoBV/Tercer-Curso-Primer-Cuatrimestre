53feab20f5c75f21f25a93aa0102a9082d158d5f
Diego BV <diego.baratto8@gmail.com>
2018-10-21 13:59:37 +0200

    Creada la base de los Fireworks
    Ahora mismo a los 5 segundos da un fallo de ejecución

---
+++ Física/SistemaParticula/Practica2/Particula.cpp
@@ -0,0 +1,41 @@
+#include "Particula.h"
+#include <iostream>
+
+physx::PxShape* Particula::createShape(Shape tipo, Medidas size)
+{
+	physx::PxShape* shape_;
+
+	switch (tipo)
+	{
+	case Capsule:
+		shape_ = CreateShape(physx::PxCapsuleGeometry(size.x_, size.y_));
+		break;
+	case Sphere:
+		shape_ = CreateShape(physx::PxSphereGeometry(size.x_));
+		break;
+	case Box:
+		shape_ = CreateShape(physx::PxBoxGeometry(size.x_, size.y_, size.z_));
+		break;
+	default:
+		break;
+	}
+
+	return shape_;
+}
+
+void Particula::integrate(float t)
+{
+	//Si la masa es infinita, la particula no cambia su posicion (paredes, suelos)
+	if (inverse_mass <= 0.0f) return;
+
+	//update de la posicion (e = e + v*t)
+	p += v * t;
+
+	//update de la velocidad
+	v += a * t;
+
+	//aplicamos la fuerza de rozamiento --> v = v * d^t
+	v *= powf(damping, t);
+
+	transform.p = p;
+}
---
+++ Física/SistemaParticula/Practica2/Time_Generator.h
@@ -0,0 +1,30 @@
+#pragma once
+#include "TemplatePool.h"
+#include "Particula.h"
+#include <time.h>
+
+class Time_Generator
+{
+	Particula::Shape shape_;
+	float time_inter = 0;
+	float next_period = 0;
+	float tiempo_transcurrido = 0;
+	TemplatePool<Particula> pool;
+	vector<Particula*> particles;
+	Vector3 pos;
+	const int VEL_MAX_ = 50;
+	const int VEL_MIN_ = 20;
+	const float G_ = -75;
+	const float MAX_LIFE_TIME = 3;
+	Vector3 generateRandomVel();
+	Vector3 generateRandomColor();
+	void generateNewParticle(float time);
+	bool checkLifeTime(vector<Particula*>::iterator& it);
+
+public:
+	virtual ~Time_Generator();
+	Time_Generator(Particula::Shape shp, float t, Vector3 pos = {10, 30, 0}) : shape_(shp), time_inter(t), pos(pos) { srand(time(NULL)); };
+	Time_Generator() {};
+	void update(float time);
+};
+
