bca6c82a59306d73994c477021d0b05ce6279b28
Diego BV <diego.baratto8@gmail.com>
2018-11-25 13:15:00 +0100

    WA again

--- MARP/Voraz/Ejercicio55/PlantillaJuezCentinela.cpp
+++ MARP/Voraz/Ejercicio55/PlantillaJuezCentinela.cpp
@@ -11,27 +11,39 @@
 
 using namespace std;
 
-const int MINUTOS_LIBRES = 10;
-
 struct Pelicula {
 	int horaComienzo, horaFin, duracion, index;
 
 	Pelicula(int hc, int hf, int duracion, int index) : horaComienzo(hc), horaFin(hf), duracion(duracion), index(index) {};
 };
 
+const int MINUTOS_LIBRES = 10;
+
+
 //esta el problema de que, por ejemplo, la primera duire un huevo
-int resuelve(vector<Pelicula>& peliculas, vector<Pelicula>& auxHoras) {
+int resuelve(vector<Pelicula>& peliculas/*, vector<Pelicula>& auxHoras*/) {
 	int numPelis = 1;
-	for (size_t i = 0; i < peliculas.size(); i++){
-		if (peliculas[i].index + 1 < auxHoras.size() && peliculas[i].horaFin + MINUTOS_LIBRES <= auxHoras[peliculas[i].index + 1].horaComienzo) { //si no solapa...
+	Pelicula p = peliculas[0];
+
+	for (size_t i = 1; i < peliculas.size(); i++){
+		/*if (peliculas[i].index + 1 < auxHoras.size() && peliculas[i].horaFin + MINUTOS_LIBRES <= auxHoras[peliculas[i].index + 1].horaComienzo) { //si no solapa...
 			numPelis++;
+		}*/
+		if (p.horaFin + MINUTOS_LIBRES <= peliculas[i].horaComienzo) {
+			numPelis++;
+			p = peliculas[i];
+		}
+		else { //se solapan
+			if (p.duracion > peliculas[i].duracion) {
+				p = peliculas[i]; //ve la de menor duracion
+			}
+			//else {p = p;}
 		}
 	}
-
 	return numPelis;
 };
 
-bool operator <(const Pelicula& p1, const Pelicula& p2) { return p1.duracion < p2.duracion; };
+bool operator <(const Pelicula& p1, const Pelicula& p2) { return p1.horaComienzo < p2.horaComienzo; };
 // Resuelve un caso de prueba, leyendo de la entrada la
 // configuración, y escribiendo la respuesta
 bool resuelveCaso() {
@@ -43,21 +55,22 @@ bool resuelveCaso() {
         return false;
    
 	vector<Pelicula> peliculas;
-	vector<Pelicula> auxHoras;
+
+	//vector<Pelicula> auxHoras;
 	for (size_t i = 0; i < N; i++){
 		string hc;
 		cin >> hc;
-		int hora = (hc[0] * 10 + hc[1]) * 60;
-		int minutos = hc[3] + hc[4];
+		int hora = ((hc[0] - '0') * 10 + (hc[1] - '0')) * 60;
+		int minutos = ((hc[3] - '0') * 10) + (hc[4] - '0');
 
 		int horaComienzo, duracion;
 		cin >> duracion;
 		horaComienzo = hora + minutos;
 		peliculas.push_back(Pelicula(horaComienzo, horaComienzo + duracion, duracion, i));
-		auxHoras.push_back(Pelicula(horaComienzo, horaComienzo + duracion, duracion, i));
+		//auxHoras.push_back(Pelicula(horaComienzo, horaComienzo + duracion, duracion, i));
 	}
 	sort(peliculas.begin(), peliculas.end());
-	cout << resuelve(peliculas, auxHoras) << endl;
+	cout << resuelve(peliculas/*, auxHoras*/) << endl;
 
     // escribir sol
     
