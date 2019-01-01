d885dbff494ac750c860351777d296f17d99a1d1
Diego BV <diego.baratto8@gmail.com>
2019-01-01 15:25:53 +0100

    Estudiando MARP

--- Física/Fuerzas/Practica1/ParticleGravity.cpp
+++
@@ -1,7 +0,0 @@
-#include "ParticleGravity.h"
-
-void ParticleGravity::updateForce(Particula * particle, float t)
-{
-	if (particle->getMass() <= 0.0f) return;               //tengo un lio con esto de la masa chaval
-	particle->addForce(g * particle->getMass());
-}
--- Física/Fuerzas/Practica3/ParticleGravity.cpp
+++
@@ -1,9 +0,0 @@
-#include "ParticleGravity.h"
-
-void ParticleGravity::updateForce(Particle * particle, float t)
-{
-	if (active_) {
-		if (particle->getMass() <= 0.0f) return;               //tengo un lio con esto de la masa chaval
-		particle->addForce(g * particle->getMass());
-	}
-}
